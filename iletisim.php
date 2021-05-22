bc94d4a086a11574db7c2e72bc704a51b3c6ec29
fuzunoglu <uzunoglufatih41@gmail.com>
2021-05-22 11:47:13 +0300

    Add files via upload

---
+++ iletisim.php
@@ -0,0 +1,24 @@
+<?php 
+
+		
+                   echo "Müşteri Adı:  ". $_POST['musteri']; 
+				   echo "<br>";
+				   echo "Telefon Numarası:  ".$_POST['telefon'];
+				   echo "<br>";
+				   
+				   echo "Email:  ".$_POST['email']; 
+				   echo "<br>";
+				   echo "Cinsiyet:  ".$_POST['cins']; 
+				   echo "<br>";
+				   echo "İletişim Nedeni:  ".$_POST['neden'];
+				   echo "<br>";
+				   
+				   echo "Tarih:  ".$_POST['tarih']; 
+				   echo "<br>";
+				   echo "Yaş:  ".$_POST['yas']; 
+				   echo "<br>";
+				   echo "Özel İstekler:  ".$_POST['yorum']; 
+				    
+				   
+				   
+?>
